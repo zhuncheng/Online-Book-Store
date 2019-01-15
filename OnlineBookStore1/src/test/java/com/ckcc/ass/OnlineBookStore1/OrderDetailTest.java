e01acbaa11eef9f235cdb77aed7bbe83ae63a259
Chum Thea <chumthea468@gmail.com>
2019-01-15 14:07:15 +0700

    fixed on OrderDetailTest

--- OnlineBookStore1/src/test/java/com/ckcc/ass/OnlineBookStore1/OrderDetailTest.java
+++ OnlineBookStore1/src/test/java/com/ckcc/ass/OnlineBookStore1/OrderDetailTest.java
@@ -14,37 +14,10 @@ import entities.OrderDetail;
 
 public class OrderDetailTest {
 	
-	OrderDetail order;
-	@Before
-	public void init() {
-		order = new OrderDetail();
-	}
-	
-	
 	@Test
-	public void getTotaltest() {
+	public void getSubTotaltest() {
 		OrderDetail orderDetail = new OrderDetail(1, 10, 10, 10);
 		assertEquals(90.0, orderDetail.getSubTotal(), 0.0);
 	}
 
-//	@Test
-//	public void test1() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchMethodException {
-//		final Field field1 = order.getClass().getDeclaredField("qty");
-//		field1.setAccessible(true);
-//		final Field field2 = order.getClass().getDeclaredField("qty");
-//		field2.setAccessible(true);
-//		final Field field3 = order.getClass().getDeclaredField("qty");
-//		field3.setAccessible(true);
-//		
-//		final Double qty = 10.0;
-//		final Double price = 10.0;
-//		final Double discount = 10.0;
-//		
-//		field1.set(order, qty);
-//		field2.set(order, price);
-//		field3.set(order, discount);
-//		
-//		final Method method = order.getClass().getDeclaredMethod("total", null);
-//		method.setAccessible(true);
-//	}
 }
